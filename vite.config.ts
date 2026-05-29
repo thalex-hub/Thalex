002529c48c91fa862e0190178eb8927ddf9990e7
thalex-hub <info.vinasglobal@gmail.com>
2026-05-30 06:45:18 +0700

    build: increase chunkSizeWarningLimit
    Adjust the rollup chunk size warning threshold to 3000kb to accommodate large vendor bundles.

--- vite.config.ts
+++ vite.config.ts
@@ -15,7 +15,7 @@ export default defineConfig(({mode}) => {
     build: {
       outDir: 'dist',
       emptyOutDir: true,
-      chunkSizeWarningLimit: 1500,
+      chunkSizeWarningLimit: 3000,
       rollupOptions: {
         output: {
           manualChunks: {
